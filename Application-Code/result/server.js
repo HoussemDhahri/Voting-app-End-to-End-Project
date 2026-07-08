36aebb0b86874d8117fee7a0d8d6a26245a32781
HoussemDhahri <dhahri_houssem@hotmail.com>
2026-07-08 20:35:14 +0100

    fix server.js

--- Application-Code/result/server.js
+++ Application-Code/result/server.js
@@ -18,7 +18,11 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: 5432
 });
 
 async.retry(
